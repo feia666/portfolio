ffc57ab82c654c440cbbc5d65ac86869eecf2679
feia666 <feiashao@gmail.com>
2020-02-13 23:11:07 -0800

    new portfolio

---
+++ docs/precache-manifest.82c94caffaf358466490040eb83ac0a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372cd884c52f88fea0b1e57f0131a135",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2385323ac3cad805f1ff",
+    "url": "/portfolio/static/css/main.3d6bb6ec.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "2385323ac3cad805f1ff",
+    "url": "/portfolio/static/js/main.44401eae.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
